4f2d446e1724e2bf3030e5586e89733c2eb467b2
unknown <rosso.2001015@studenti.uniroma1.it>
2024-04-29 11:53:25 +0200

    Svuotata la cartella

--- homework2/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    if ( isset($_SESSION["nome"]) ) // Verifico se vi e' una sessione aperta
-        require_once 'cancellaSessione.php';
-    
-    header("Location: accedi.php");
-?>
